5b71f052d816e2eb1883b7657d76a8a403727f49
aneylon <arlen.m.neylon@gmail.com>
2018-08-29 18:29:31 -0600

    Add test and basic route for presentations.

--- test/index.test.js
+++ test/index.test.js
@@ -8,6 +8,21 @@ const server = require('../index')
 
 chai.use(chaiHttp)
 
+describe('/GET presentation', () => {
+  it('Should get a list of presentations.', (done) => {
+    chai.request(server)
+      .get('/presentation')
+      .end((err, res) => {
+        res.should.have.status(200)
+        res.body.should.be.a('array')
+        res.body.length.should.be.above(0)
+        res.body[0].should.have.property('id')
+        res.body[0].should.have.property('slides')
+        done()
+      })
+  })
+})
+
 describe('/GET slide', () => {
   it('Should get a list of slides.', (done) => {
     chai.request(server)
